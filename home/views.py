9758700a265328449a95618d2a35ff533d845525
C0mplex <gaybullayevodiljon@gmail.com>
2023-10-20 19:13:10 +0500

    q

--- home/views.py
+++ home/views.py
@@ -1,5 +1,11 @@
+from django.http import FileResponse
 from django.shortcuts import render
 
 # Create your views here.
 def home(request):
     return render(request, 'index.html')
+
+def send_adstxt(request):
+    temp = open('static/ads.txt', 'rb')
+    response = FileResponse(temp)
+    return response
